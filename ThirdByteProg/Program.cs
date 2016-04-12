bf9b77feae3a5fef2ae585c78cd808d972c60045
Maxim <m.evsyukov@2gis.ru>
2016-04-12 19:26:03 +0600

    Все задачи

---
+++ ThirdByteProg/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int x, z;
+            x = Convert.ToByte(a);
+            z = x & (x/2);
+            Console.WriteLine(z);
+            Console.ReadKey();
+
+        }
+    }
+}
