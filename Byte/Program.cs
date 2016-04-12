20f7908bbb6b671132a9e0de43f34c1b6e767b57
Maxim <m.evsyukov@2gis.ru>
2016-04-12 18:31:40 +0600

    Первое и второе задание

---
+++ Byte/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string i = Console.ReadLine();
+            int a, b;
+            int c, d;
+            a = Convert.ToByte(n);
+            b = Convert.ToByte(i);
+            c = (a >> b);
+            d = (c & 1);
+            Convert.ToString(d);
+            Console.WriteLine(d);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ a+b/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            int c = Convert.ToInt32(a);
+            int d = Convert.ToInt32(b);
+            int x = c + d;
+            Console.WriteLine(x);
+            Console.ReadKey();
+
+        }
+    }
+}
