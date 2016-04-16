18d396187c4ea17ab59e710bcf175e2f596f828e
Maxim <m.evsyukov@2gis.ru>
2016-04-16 17:56:21 +0600

    ДЗ

---
+++ 14.04.16/For_Task1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int min = 0;
+            int max = 0;
+            int even = 0;
+            int odd = 1;
+            for (int i = 0; i < n; i++)
+            {
+                int value = Convert.ToInt32(Console.ReadLine());
+                if (i == 0)
+                {
+                    min = value;
+                    max = value;
+                }
+                else
+                {
+                    if (min > value)
+                    {
+                        min = value;
+                    }
+                    if (max < value)
+                    {
+                        max = value;
+                    }
+                }
+                if (value % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd = value * odd;
+                }
+                sum = sum + value;
+                Console.WriteLine("Сумма чисел: " + sum);
+                Console.WriteLine("Минимальное число: " + min);
+                Console.WriteLine("Максимальное число: " + max);
+                Console.WriteLine("Количество четных чисел: " + even);
+                Console.WriteLine("Произведение нечетных чисел: " + odd);
+            }
+        }
+    }
+}
---
+++ 14.04.16/Massive_Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int [n];
+            int a;
+            for (int i = 0; i < n; i++)
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                numbers[i] = a;
+            }
+
+            Array.Sort(numbers);
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
