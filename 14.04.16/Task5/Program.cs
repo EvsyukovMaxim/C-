986231966c2e7c890eb7bd010333fa4c74a7ad45
Maxim <m.evsyukov@2gis.ru>
2016-04-24 00:52:52 +0600

    .

--- 14.04.16/Task5/Program.cs
+++ 14.04.16/Task5/Program.cs
@@ -12,27 +12,28 @@ namespace For_Task1
         {
             //Инициализация матриц
             int n = Convert.ToInt32(Console.ReadLine());
-            int[,] matrix1 = new int[n, n];
-            int[,] matrix2 = new int[n, n];
-            int[,] matrix3 = new int[n, n];
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[,] matrix1 = new int[n, m];
+            int[,] matrix2 = new int[n, m];
+            int[,] matrix3 = new int[n, m];
             Random rnd = new Random();
 
             for (int i = 0; i < n; i++)
             {
                 int a = rnd.Next();
-                for (int j = 0; j < n; j ++)
+                for (int j = 0; j < m; j++)
                 {
                     matrix1[i, j] = a;
                     matrix2[i, j] = a;
                     matrix3[i, j] = a + a;
-                }   
+                }
             }
 
             //Выводим на консоль заполненную 1-ую матрицу
             Console.WriteLine("first Matrix");
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < m; j++)
                 {
                     Console.Write(matrix1[i, j] + " ");
                 }
@@ -43,7 +44,7 @@ namespace For_Task1
             Console.WriteLine("second Matrix");
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < m; j++)
                 {
                     Console.Write(matrix2[i, j] + " ");
                 }
@@ -54,7 +55,7 @@ namespace For_Task1
             Console.WriteLine("the third SUM Matrix");
             for (int i = 0; i < n; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < m; j++)
                 {
                     Console.Write(matrix3[i, j] + " ");
                 }
