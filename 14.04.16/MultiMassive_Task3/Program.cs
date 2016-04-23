ad4292303e05b78b84b87b008d4718190c7a7e2a
Maxim <m.evsyukov@2gis.ru>
2016-04-23 16:37:40 +0600

    /

---
+++ 14.04.16/MultiMassive_Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            int[,] matrix1 = new int[n, n];
+            int[][] matrix2 = new int[n][];
+
+            Console.Write(matrix1[0,0]);
+        }
+    }
+}
---
+++ 19.04.16/Methods1_Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods1_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            decimal[] numbers = new decimal[n];
+            decimal a;
+            for (int i = 0; i < n; i++)
+            {
+                a = ReadDec();
+                numbers[i] = a;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                PrintDec(numbers[i]);
+            }
+        }
+
+        //static int ReadInt()
+        //{
+        //    return Convert.ToInt32(Console.ReadLine());
+        //}
+
+        static decimal ReadDec()
+        {
+            return Convert.ToDecimal(Console.ReadLine());
+        }
+
+        static void PrintDec(decimal number)
+        {
+            Console.Write(number + " ");
+        }
+    }
+}
