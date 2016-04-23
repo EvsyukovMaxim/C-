81852655bfa1277f00a8d225f7aef4338722c8ae
Maxim <m.evsyukov@2gis.ru>
2016-04-24 00:04:04 +0600

    Серега, прости ...

--- 14.04.16/MultiMassive_Task3/Program.cs
+++ 14.04.16/MultiMassive_Task3/Program.cs
@@ -10,20 +10,29 @@ namespace For_Task1
     {
         static void Main(string[] args)
         {
+            //Инициализация матриц
             int n = Convert.ToInt32(Console.ReadLine());
             int m = Convert.ToInt32(Console.ReadLine());
             int[,] matrix1 = new int[n, m];
+            int[,] matrix2 = new int[n, m];
+            int[,] matrix3 = new int[n, m];
+            Random rnd = new Random();
 
             for (int i = 0; i < n; i++)
             {
-                int a = Convert.ToInt32(Console.ReadLine());
+                int a = rnd.Next();
                 for (int j = 0; j < m; j ++)
                 {
                     matrix1[i, j] = a;
+                    matrix2[i, j] = a;
+                    matrix3[i, j] = a + a;
+
                 }
                 
             }
 
+            //Выводим на консоль заполненную 1-ую матрицу
+            Console.WriteLine("first Matrix");
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
@@ -32,6 +41,31 @@ namespace For_Task1
                 }
                 Console.WriteLine();
             }
+
+            //Выводим на консоль заполненную 2-ую матрицу
+            Console.WriteLine("second Matrix");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+            //Печатаем итог сложения двух матриц
+            Console.WriteLine("the third SUM Matrix");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix3[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
