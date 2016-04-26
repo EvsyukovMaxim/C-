80893f0397bae007ba00ad6d35557c37e93ea808
Maxim <m.evsyukov@2gis.ru>
2016-04-26 13:58:00 +0600

    Сделал все три домашки)

--- 19.04.16/Task3_Number_-1/Program.cs
+++ 19.04.16/Task3_Number_-1/Program.cs
@@ -12,23 +12,22 @@ namespace Task3_Number__1
         {
             List<int> integers = new List<int>();
             List<int> integersDouble = new List<int>();
-            int n = Convert.ToInt32(Console.ReadLine());
+            int n = 0;
 
             while (n != -1)
             {
                 n = Convert.ToInt32(Console.ReadLine());
                 if (integers.Contains(n))
                 {
-
                     integersDouble.Add(n);
                 }
                 integers.Add(n);
             }
 
+            Console.Write("Это дублирующиеся значения: ");
             for (int i = 0; i < integersDouble.Count; i++)
             {
-                Console.Write("Это дублирующиеся значения: " + integersDouble[i] + " ");
-
+                Console.Write(integersDouble[i] + " ");
             }
 
         }
