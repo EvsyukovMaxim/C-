0b96f8924861a11b3cc6fa2bc745a5aaf35b50bf
Maxim <m.evsyukov@2gis.ru>
2016-05-11 20:19:26 +0600

    6-ое задание

---
+++ 05.05.16/HomeWork_BankClient/BaseAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_BankClient
+{
+    public abstract class BaseAccount
+    {
+        protected int _currentSum;
+        protected bool _isActive;
+
+        public BaseAccount()
+        {
+            _currentSum = 100000;
+            _isActive = true;
+        }
+
+        public bool CloseAccount()
+        {
+            if (_currentSum <= 0)
+            {
+                _isActive = false;
+                return _isActive;
+            }
+            else
+            {
+                return _isActive;
+            }
+        }
+        public int ReturnCurrentSum { get {return _currentSum; } }
+    }
+}
---
+++ 05.05.16/HomeWork_BankClient/StandartClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_BankClient
+{
+    class StandartClient
+    {
+        public void addAccount (BaseAccount account)
+        {
+            List<BaseAccount> accountsList = new List<BaseAccount>();
+            if (account < 4)
+            {
+                accountsList.Add(account);
+            }
+        }
+    }
+}
