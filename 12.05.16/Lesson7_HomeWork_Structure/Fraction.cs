41b0de43e415ef35793ff7895d0f1e0f2949b00a
Maxim <m.evsyukov@2gis.ru>
2016-05-15 21:17:48 +0600

    7-ое Задание

---
+++ 12.05.16/Lesson7_HomeWork_Structure/Fraction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_HomeWork_Structure
+{
+    //class Fraction
+    //{
+        public struct FractionXY
+        {
+            public double X { get; private set; }
+            public double Y { get; private set; }
+            public Cleavage(double x, double y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+    //}
+}
