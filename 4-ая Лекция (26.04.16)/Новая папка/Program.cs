584c8244b2c465bb080ecf591540084cb851cd3c
Maxim <m.evsyukov@2gis.ru>
2016-05-18 10:19:20 +0600

    .

---
+++ 4-ая Лекция (26.04.16)/Новая папка/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telephone myTelephone = new Telephone();
+
+            Console.ReadKey();
+            Console.WriteLine($"{ myTelephone.FullNumber}");
+        }
+    }
+}
