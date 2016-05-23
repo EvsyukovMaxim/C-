44df35fabd61a9fa238b20f424fc90cab3088347
Maxim <m.evsyukov@2gis.ru>
2016-05-23 22:26:39 +0600

    /../

--- 7-ая Лекция (12.05.16)/Lesson7_HomeWork_Structure/Program.cs
+++ 7-ая Лекция (12.05.16)/Lesson7_HomeWork_Structure/Program.cs
@@ -44,7 +44,7 @@ namespace Lesson7_HomeWork_Structure
 
             for (int i = 0; i < randomFractions.Length; i++)
             {
-                ((Fraction)sum) = ((Fraction)sum).Addition((Fraction)randomFractions[i]);
+                sum = sum.Addition(randomFractions[i]);
             }
 
             Console.WriteLine("Сумма всех дробей равна: " + sum);
