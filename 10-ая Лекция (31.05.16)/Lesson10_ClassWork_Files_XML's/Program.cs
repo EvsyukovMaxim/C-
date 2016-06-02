cb6fe84b38f34fa3922f040e74d22eae4b2e2253
Maxim <m.evsyukov@2gis.ru>
2016-06-02 09:23:29 +0600

    10-ое Задание

---
+++ 10-ая Лекция (31.05.16)/Lesson10_ClassWork_Files_XML's/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lesson10_ClassWork_Files_XML_s
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter sw = new StreamWriter(@"D:\мой файлик.txt"))
+            for (int i = 0; i < n; i++)
+            {
+                string number = Console.ReadLine();
+                sw.WriteLine(number);
+            }
+
+            using (StreamReader sr = new StreamReader(@"D:\мой файлик.txt"))
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+        }
+    }
+}
