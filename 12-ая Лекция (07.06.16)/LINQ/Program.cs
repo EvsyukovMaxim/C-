23a6359846a6e4291f7c1e266cc2bf3d4bc716e3
Maxim <m.evsyukov@2gis.ru>
2016-06-07 18:25:44 +0600

    UnitTests

---
+++ 12-ая Лекция (07.06.16)/LINQ/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 1, 5, 3, 7, 3, 9, 6, 4, 3, 2 };
+
+            var filteredNumbers = numbers.Where(u => u % 2 == 0 || u > 5).OrderByDescending(u => u);
+
+            foreach (var number in filteredNumbers)
+            {
+                Console.WriteLine(numbers);
+            }
+        }
+    }
+}
